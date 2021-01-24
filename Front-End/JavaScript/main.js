938ce5d3ff7261bf067d3cef365875f6e72ca594
psp290 <pratikpatil290@gmail.com>
2021-01-24 15:59:12 +0530

    Front end

--- Front-End/JavaScript/main.js
+++ Front-End/JavaScript/main.js
@@ -9,13 +9,13 @@ function changemode(){
     if(text=="Light Mode")
     {
         document.getElementById('changemode').value="Dark Mode";
-        document.getElementById('changemode').style="color:white";
-        document.getElementById('changemode').style="background-color:black";
+        document.getElementById('changemode').style="color:white;margin-top: 10px;";
+        document.getElementById('changemode').style="background-color:black;margin-top: 10px;";
     }
     else
     {
         document.getElementById('changemode').value="Light Mode";
-        document.getElementById('changemode').style="color:black";
+        document.getElementById('changemode').style="color:black;margin-top: 10px;";
         
     }
 }
